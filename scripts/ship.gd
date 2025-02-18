0324640f9f99911b7835601c00587d741586ca92
aurelatioukpe <aurel.atioukpe@epitech.eu>
2025-02-18 11:47:23 +0100

    add: main menu, play and quit button implementation and replace the ship by the good one.

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
 	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
